d68650f7de6df62a74678f7794f1882dc92549be
Rob Haney <robert.a.haney@gmail.com>
2022-07-01 15:50:48 -0400

    Add files via upload
    Added check for correct file format.

--- count_gff_features.py
+++ count_gff_features.py
@@ -2,6 +2,7 @@ import sys
 
 features = {}
 
+complete = True
 # open and read gff file 
 with open(sys.argv[1]) as fhand:
 	for line in fhand:
@@ -10,14 +11,19 @@ with open(sys.argv[1]) as fhand:
 			continue
 		# split columns 
 		temp = line.split()
+		# check that line contains at least 9 columns, as specified in gff definition
+		if len(temp) < 9:
+			print "\nCheck file format: gff file should have at least nine columns\n"
+			complete = False
+			break
 		# add to dict or increment
 		if temp[2] not in features:
 			features[temp[2]] = 1
 		else:
 			features[temp[2]] += 1
 
-
-print 		
-print "Features present in file", sys.argv[1], "and number of entries:"
-for feature in sorted(features):
-	print feature + ": " + "\t" + str(features[feature])
+if complete:
+	print 		
+	print "Features present in file", sys.argv[1], "and number of entries:"
+	for feature in sorted(features):
+		print feature + ": " + "\t" + str(features[feature])
